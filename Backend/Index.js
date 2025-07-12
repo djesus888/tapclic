85f1fcae2843b66188bcceb9fc6a1f280f8bc3eb
Jesús Diaz <divijeal@gmail.com>
2025-07-12 18:42:26 -0400

    index.js

---
+++ Backend/Index.js
@@ -0,0 +1,11 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('¡Hola desde el backend de Tapclic!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
